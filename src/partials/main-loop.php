d25cf79ee092ed5f9d8832237b57200180a777b0
JeffPlo <jewgeni.plotnikow@gmx.de>
2017-10-03 22:04:16 +0200

    Improve partials loading.

--- src/partials/main-loop.php
+++ src/partials/main-loop.php
@@ -1,14 +1,14 @@
 <?php
+	global $post;
+
+	$homeId = (int) get_option( 'page_on_front' );
+	$childPage = is_page() && $post->post_parent === $homeId;
 ?>
 
 <div class="row">
     <div class="small-12 columns">
 		<?php
-        if ( is_page( 'impressum' ) ) {
-	        the_content();
-        }
-		else {
-			$homeId = get_option( 'page_on_front' );
+        if ( is_front_page() || $childPage ) {
 			$pages  = get_pages( array(
 				'child_of'    => $homeId,
 				'parent'      => $homeId,
@@ -19,9 +19,18 @@
 			if ( !empty( $pages ) ) {
 				foreach ( $pages as $page ) {
 					echo '<div class="page-anchor-' . $page->ID . '"></div>';
+
+					if ( has_post_thumbnail( $page->ID ) ) {
+						$image = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'full' );
+						echo '<style> .bg-id-' . $page->ID . ' { background-image: url(' . $image[0] .'); } </style>';
+					}
+
 					echo do_shortcode( $page->post_content );
 				}
 			}
+		}
+		else {
+			the_content();
 		}
 		?>
     </div>
